1d96596d6a80c00b9456503df063ded0928a7980
JunHyuckWoo <wjh0470@gmail.com>
2018-07-09 16:36:39 -0400

    Get two input data set

--- 0709tensorDataset.py
+++ 0709tensorDataset.py
@@ -2,16 +2,17 @@ import tensorflow as tf
 
 
 def gen():
-    for i in range(10):
-        yield i
+    for i, j in zip(range(10, 20), range(10)):
+        yield (i, j)
 
 
-dataset = tf.data.Dataset.from_generator(gen, tf.float32)\
+dataset = tf.data.Dataset.from_generator(gen, (tf.float32, tf.float32))\
     .make_one_shot_iterator()\
     .get_next()
 
 
 with tf.Session() as sess:
-    _data = sess.run(dataset)
-    print(_data)
+    for _ in range(10):
+        _data = sess.run(dataset)
+        print(_data)
 
