7e8047c28aa324de3c7fdd641267e8ea25f5a250
JunHyuckWoo <wjh0470@gmail.com>
2018-07-11 19:52:13 -0400

    Add cost, train line

--- 0711diabetes.py
+++ 0711diabetes.py
@@ -9,3 +9,8 @@ y = tf.placeholder(tf.float32, shape=[None, classes])
 
 w = tf.Variable(tf.random_normal([feature, classes]), dtype=tf.float32, name='Weight')
 b = tf.Variable(tf.random_normal([classes], dtype=tf.float32, name='bias'))
+
+hypothesis = tf.sigmoid(tf.matmul(x, w) + b)
+cost = -tf.reduce_mean(y * tf.log(hypothesis) + (1 - y) * tf.log(1 - hypothesis))
+train = tf.train.GradeientDescentOptimizer(alpha).minimize(cost)
+
