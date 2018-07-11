26c94e1ac05539fffa5681d3e977a9405ef99a0b
JunHyuckWoo <wjh0470@gmail.com>
2018-07-11 18:53:41 -0400

    Add weight, bias

--- 0711diabetes.py
+++ 0711diabetes.py
@@ -7,4 +7,5 @@ alpha = 0.01
 x = tf.placeholder(tf.float32, shape=[None, feature])
 y = tf.placeholder(tf.float32, shape=[None, classes])
 
-
+w = tf.Variable(tf.random_normal([feature, classes]), dtype=tf.float32, name='Weight')
+b = tf.Variable(tf.random_normal([classes], dtype=tf.float32, name='bias'))
