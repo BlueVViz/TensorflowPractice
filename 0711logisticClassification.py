314db40bcb96e28d39a0c872f5e90ee3589bc47a
JunHyuckWoo <wjh0470@gmail.com>
2018-07-11 16:02:27 -0400

    Add alpha rate variable

--- 0711logisticClassification.py
+++ 0711logisticClassification.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 # Variable
 x_data = [[1, 2], [2, 3], [3, 1], [4, 3], [5, 3], [6, 2]]
 y_data = [[0], [0], [0], [1], [1], [1]]
-
+alpha = 0.1
 
 x = tf.placeholder(tf.float32, shape=[None, 2])
 y = tf.placeholder(tf.float32, shape=[None, 1])
@@ -13,3 +13,4 @@ bias = tf.Variable(tf.random_normal([1]), name='bias')
 
 hypothesis = tf.sigmoid(tf.matmul(x, weight) + bias)
 cost = -tf.reduce_mean(y * tf.log(hypothesis)) + (1 - y) * tf.log(1 - hypothesis)
+
