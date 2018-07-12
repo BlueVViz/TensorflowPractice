1123a64a8934028975ee2ba45b0de291f9f9657b
JunHyuckWoo <wjh0470@gmail.com>
2018-07-11 20:03:13 -0400

    Variable initialize

--- 0711diabetes.py
+++ 0711diabetes.py
@@ -12,5 +12,9 @@ b = tf.Variable(tf.random_normal([classes], dtype=tf.float32, name='bias'))
 
 hypothesis = tf.sigmoid(tf.matmul(x, w) + b)
 cost = -tf.reduce_mean(y * tf.log(hypothesis) + (1 - y) * tf.log(1 - hypothesis))
-train = tf.train.GradeientDescentOptimizer(alpha).minimize(cost)
+train = tf.train.GradientDescentOptimizer(alpha).minimize(cost)
 
+with tf.Session() as sess:
+    sess.run(tf.global_variable_initializer())
+
+    
